29fd61fd093d4c812188f33ca0c095405926b3d3
YoonJunHyeon <qmdldos@gmail.com>
2023-10-24 16:27:22 +0900

    js의 모듈화 주의점은 호화성에 주의한다.

---
+++ public/data.js
@@ -0,0 +1,7 @@
+ // 예제 데이터
+export const messages = [
+  { type: "left", text: "안녕하세요!", author: "주인공" },
+  { type: "right", text: "안녕하세요, 주인공님!", author: "사람1" },
+  // ... 다른 메시지들
+];
+
--- public/script.js
+++ public/script.js
@@ -1,9 +1,4 @@
-// 예제 데이터
-const messages = [
-  { type: "left", text: "안녕하세요!", author: "주인공" },
-  { type: "right", text: "안녕하세요, 주인공님!", author: "사람1" },
-  // ... 다른 메시지들
-];
+import { messages } from "./data.js";
 
 const container = document.querySelector('.container');
 
