933c9081c8ad60935f81a1b3527afbcf05b9dd08
YoonJunHyeon <qmdldos@gmail.com>
2023-10-24 15:14:05 +0900

    서버 로직 작성

---
+++ app.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const fs = require('fs');
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/' || req.url === '/index.html') {
+        fs.readFile('./public/index.html', 'utf8', (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+                return;
+            }
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        });
+    } else if (req.url.endsWith('.css')) {
+        fs.readFile(`./public${req.url}`, 'utf8', (err, data) => {
+            if (err) {
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.end('Not Found');
+                return;
+            }
+            res.writeHead(200, { 'Content-Type': 'text/css' });
+            res.end(data);
+        });
+    } else if (req.url.endsWith('.js')) {
+        fs.readFile(`./public${req.url}`, 'utf8', (err, data) => {
+            if (err) {
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.end('Not Found');
+                return;
+            }
+            res.writeHead(200, { 'Content-Type': 'text/javascript' });
+            res.end(data);
+        });
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
