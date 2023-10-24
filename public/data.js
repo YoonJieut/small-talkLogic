68459a21216546893dd6a42c8f257bba98ad948d
YoonJunHyeon <qmdldos@gmail.com>
2023-10-24 17:01:18 +0900

    narration 수정

--- public/data.js
+++ public/data.js
@@ -3,5 +3,10 @@ export const messages = [
   { type: "left", text: "안녕하세요!", author: "주인공" },
   { type: "right", text: "안녕하세요, 주인공님!", author: "사람1" },
   // ... 다른 메시지들
+  {
+    type: "narration",
+    content: "이 부분은 해설입니다. 주요 사건의 배경이나 설명을 담고 있습니다."
+  },
 ];
+
 
--- public/script.js
+++ public/script.js
@@ -1,20 +1,29 @@
-import { messages } from "./data.js";
+import { messages } from './data.js';
 
-const container = document.querySelector('.container');
+const container = document.querySelector(".container");
 
 messages.forEach(message => {
-  const messageDiv = document.createElement('div');
-  messageDiv.classList.add('message', message.type);
+    const messageDiv = document.createElement('div');
+    messageDiv.classList.add('message', message.type);
 
-  const profileDiv = document.createElement('div');
-  profileDiv.classList.add('profile');
-  profileDiv.innerText = message.author[0];  // 첫 글자만
+    if (message.type === 'narration') {
+        // "해설" 텍스트의 경우
+        const narrationText = document.createElement('div');
+        narrationText.classList.add('narration');
+        narrationText.innerText = message.content;
+        messageDiv.appendChild(narrationText);
+    } else {
+        const profileDiv = document.createElement('div');
+        profileDiv.classList.add('profile');
+        profileDiv.innerText = message.author[0];  // 첫 글자만
 
-  const textBox = document.createElement('div');
-  textBox.classList.add('text-box');
-  textBox.innerText = message.text;
+        const textBox = document.createElement('div');
+        textBox.classList.add('text-box');
+        textBox.innerText = message.text;
 
-  messageDiv.appendChild(profileDiv);
-  messageDiv.appendChild(textBox);
-  container.appendChild(messageDiv);
+        messageDiv.appendChild(profileDiv);
+        messageDiv.appendChild(textBox);
+    }
+
+    container.appendChild(messageDiv);
 });
